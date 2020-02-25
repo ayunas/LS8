a4dc3f5fda81bf889990b07b785e26c230d293c0
Amir Yunas <ayunas@gmail.com>
2020-02-25 18:14:24 +0100

    implemented the LDI instruction

--- ls8/instructions.py
+++ ls8/instructions.py
@@ -1,4 +1,4 @@
-instructions = {
+inst = {
     "LDI" : 0b10000010,
     "PRN" : 0b01000111,
     "HLT" : 0b00000001
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -2,12 +2,15 @@ import sys
 from itertools import dropwhile
 sys.path.append('./examples')
 
+from instructions import inst
+
 class LS8:
     def __init__(self):
         self.ram = [0]*256 #8 bit processor can handle 256 bytes in memory
         self.registers = [0]*8
         self.pc = 0
-    
+        self.mar = None #Memory Address Register, holds the memory address to read/write
+
     def load(self):
         filename = input("enter the LS8 program you wish to run: ")
         try:
@@ -18,19 +21,62 @@ class LS8:
             print(f"Could not open/read file {filename}. exiting...")
             sys.exit(1)
         
-        prog_comments = [*dropwhile(lambda l : l.startswith('#') or l == '\n',data)]
-        prog = [byte.split('#')[0].strip() for byte in prog_comments]
-        program = [b for b in prog if b != '']
+        clear_header = [*dropwhile(lambda l : l.startswith('#') or l == '\n',data)]
+        clear_comments = [byte.split('#')[0].strip() for byte in clear_header]
+        program = [b for b in clear_comments if b != '']
         print(program, '\n')
 
         address = self.ram.index(0) #sets address to first empty space in memory
         
-        for byte in program:
-            self.ram[address] = byte
+        for byte_str in program:
+            self.ram[address] = int(byte_str,2)
             address += 1
         
         print(self.ram)
+        print('program loaded into RAM successfully')
+    
+    def ram_read(self):
+        print('ram_read')
+        self.pc += 1
+        return self.ram[self.pc]
+    
+    def reg_write(self,reg,data):
+        print('reg_write')
+        self.registers[reg] = data
+        print(self.registers)
+        self.pc += 1
+    
+    def run(self):
+        self.load()
+        halted = False
+
+        while halted == False:
+            instruction = self.ram[self.pc]
+            print(instruction == inst['LDI'])
+            if instruction == inst['LDI']:
+                reg = self.ram_read()
+                data = self.ram_read()
+                print(reg,data)
+                self.reg_write(reg,data)
+
+            print(self.registers)
+            break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
 ls8 = LS8()
-
-ls8.load()
+ls8.run()
