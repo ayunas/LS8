fcc9b7a33a51509108f82a9855d05741b809c4c9
Amir Yunas <ayunas@gmail.com>
2020-02-25 17:41:33 +0100

    finished load function to load ls8 assembly program into memory

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,8 +5,8 @@ sys.path.append('./examples')
 class LS8:
     def __init__(self):
         self.ram = [0]*256 #8 bit processor can handle 256 bytes in memory
-        self.register = [0]*8
-        self.pc = self.register[-1]
+        self.registers = [0]*8
+        self.pc = 0
     
     def load(self):
         filename = input("enter the LS8 program you wish to run: ")
@@ -19,13 +19,17 @@ class LS8:
             sys.exit(1)
         
         prog_comments = [*dropwhile(lambda l : l.startswith('#') or l == '\n',data)]
-        program = [byte.split('#')[0].strip() for byte in prog_comments]
-        program = [b for b in program if b != '']
-        print(program)
+        prog = [byte.split('#')[0].strip() for byte in prog_comments]
+        program = [b for b in prog if b != '']
+        print(program, '\n')
 
-
-
-
+        address = self.ram.index(0) #sets address to first empty space in memory
+        
+        for byte in program:
+            self.ram[address] = byte
+            address += 1
+        
+        print(self.ram)
 
 ls8 = LS8()
 
