1b7a50b0d88f2cc4aabc901e1ddc90af5defec61
Amir Yunas <ayunas@gmail.com>
2020-03-01 10:07:54 -0500

    modified SHR and SHL alu ops to use shorthand syntax of   reg[a] >>= bits and reg[a] <<= bits.  works correctly

--- ls8/opcodes.py
+++ ls8/opcodes.py
@@ -11,7 +11,18 @@ opc = {
     "RET" : 0b00010001,
     "IRET": 0b00010011,
     "ST"  : 0b10000100,
-    "PRA" : 0b01001000
+    "PRA" : 0b01001000,
+    "CMP" : 0b10100111,
+    "JEQ" : 0b01010101,
+    "JNE" : 0b01010110,
+    "ADDI" : 0b10100001, #2 operands, 1 ALU operation, 0 - no modifying the pc
+    "AND" : 0b10101000,
+    "OR"  : 0b10101010,
+    "XOR" : 0b10101011,
+    "NOT" : 0b01101001,
+    "SHL" : 0b10101100,
+    "SHR" : 0b10101101,
+    "MOD" : 0b10100100,
 }
 
 branch_table = {
